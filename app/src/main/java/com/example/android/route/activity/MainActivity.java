58d2d0736b1ac7191c625008c00f907d2c8c1a6e
gracehkpark <gracehkpark@gmail.com>
2015-09-07 13:14:24 +0900

    init

--- app/src/main/java/com/example/android/route/activity/MainActivity.java
+++ app/src/main/java/com/example/android/route/activity/MainActivity.java
@@ -8,7 +8,6 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
